de2bb8cb162251ab4deb96cbedecd358d37f0d62
Mahmoud Nashaat <mahmmoudnashat801@gmail.com>
2025-12-25 23:25:37 +0200

    Events and Annoucement   Module Implementation

---
+++ WebApplication1/Controllers/AnnouncementsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.UniversityModels;
+
+namespace WebApplication1.Controllers
+{
+   
+    public class AnnouncementsController : Controller
+    {
+        private readonly UniversityContext _context;
+        public AnnouncementsController(UniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Announcements
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Announcements.OrderByDescending(a => a.CreatedDate).ToListAsync());
+        }
+
+        [Authorize(Roles = "Admin,Staff")]
+
+        // GET: Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin,Staff")]
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(announcement);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(announcement);
+        }
+
+        // Optional: Delete or Edit
+    }
+}
---
+++ WebApplication1/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.UniversityModels;
+
+[Authorize(Roles = "Student")]
+public class EventsController : Controller
+{
+    private readonly UniversityContext _context;
+    public EventsController(UniversityContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Events.OrderBy(e => e.EventDate).ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Event evt)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(evt);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(evt);
+    }
+}
